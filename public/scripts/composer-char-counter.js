a80675f3a67b00bc9b9f6aa98bacf93d969744bc
DLindeblom <drdlindeblom@gmail.com>
2022-06-21 21:57:47 +0000

    got the numbers turning red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,18 @@
-  $(document).ready(function(){
-    let maxlength = 140;
-     $("#tweet-text").on("input", function() {
-      // console.log(this);
-      let currentlength = $(this).val().length;
-      let charsLeft = maxlength-currentlength;
-      
-      let counter = $(this).next().children().closest("output").text(charsLeft);
-     
-      if(!charsLeft) {
-        let element = document.getElementsByClassName("counter")
-        element.classList.add("negative")
-      }
-     });
-   });
+$(document).ready(function() {
+  let maxlength = 10;
+  $("#tweet-text").on("input", function() {
+    // console.log(this);
+    let currentlength = $(this).val().length;
+    let charsLeft = maxlength - currentlength;
+
+    const counter = $(this).next().children().closest("output").text(charsLeft);
+
+    if (charsLeft < 0) {
+      return counter.addClass("red");
+    }
+    counter.removeClass("red");
+
+  });
+
+});
 
